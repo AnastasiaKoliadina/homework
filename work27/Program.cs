829131d148872ac08479df515efeff40d7384d8f
Anastasia <anastasia7066084@mail.ru>
2022-12-14 16:01:04 +0300

    .\work27\

---
+++ work27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int SumDigNumber(int number)
+{
+    int sum = 0;
+while(number>0)
+{
+     int N = number % 10;
+     sum = sum + N;
+     number = number / 10;
+}
+return sum;
+}
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр в числе {n}= {SumDigNumber(n)}");
+
