15578dc1145f13f60ccf5b6831a43e242840fbeb
Anastasia <anastasia7066084@mail.ru>
2022-12-15 14:07:54 +0300

    work29 второй вариант задачи

--- work29/Program.cs
+++ work29/Program.cs
@@ -19,3 +19,23 @@ Console.Write(arr[i] + ",");
 Console.Write(arr[7] + "]");
 }
 PrintRandArray(СreatArray());
+
+//Еще один способ решения задачи.
+//старайтесь избегать магических чисел, например в задаче 29 вместо 8 и 7 можно заменить на:
+int[] СreatArray()
+{
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(0, 100);
+}
+return array;
+}
+void PrintRandArray(int[] arr)
+{
+Console.Write("[");
+for (int i = 0; i < arr.Length - 1; i++)
+Console.Write(arr[i] + ",");
+Console.Write(arr[arr.Length - 1] + "]");
+}
+PrintRandArray(СreatArray());
