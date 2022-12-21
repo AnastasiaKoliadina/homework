b1e4b9bd22615d421a0b3a31ff803f13efe6a9c0
Anastasia <anastasia7066084@mail.ru>
2022-12-21 11:28:28 +0300

    .\work41\

---
+++ work41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void CountNum(string[] Number)
+{
+    int count = 0;
+    foreach (string el in Number)
+    {
+        if(int.Parse(el) > 0)
+        count++;
+    }
+    Console.WriteLine($"Количество положительных чисел: {count}");
+}
+Console.WriteLine("Введите числа через запятую ");
+string[] Number = (Console.ReadLine()!).Split(',');
+CountNum(Number);
+
+
