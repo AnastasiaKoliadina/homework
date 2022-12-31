8c29b166ae47b646771fcf26bf9b88ee9338c7f9
Anastasia <anastasia7066084@mail.ru>
2022-12-31 18:21:22 +0300

    .\work66\

---
+++ work66/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNum(int m,int n)
+{
+    if(n>m) return n>m ? n + SumNum(m, n-1) : n;
+    else return m>n ? m + SumNum(m-1,n) : m;
+}
+Console.WriteLine(SumNum(15,1));
